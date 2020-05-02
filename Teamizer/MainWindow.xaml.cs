395a9e2c6172f1392f94aef5273db755576f4af3
altertango <36963238+altertango@users.noreply.github.com>
2020-05-02 14:47:20 +0200

    MyCommit

--- Teamizer/MainWindow.xaml.cs
+++ Teamizer/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 using System.Windows.Interop;
 
 namespace Teamizer {
-    /// <summary>
+    /// <summmmmmary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
